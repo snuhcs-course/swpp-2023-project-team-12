036a16d9585ae713ca12517bed4d14c574d25d33
dydghk456 <yong@sin-yonghwaui-MacBookAir.local>
2023-10-02 22:31:33 +0900

    Fix: Use authenticate function instead of serializer in LoginView function

--- django/account/views.py
+++ django/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import CustomUser
 from .serializers import UserCreateSerializer, LoginSerializer
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.views import APIView
@@ -20,15 +21,15 @@ class LoginView(APIView):
     serializer_class = LoginSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=False):
-            user = serializer.validated_data['user']
+        #serializer = self.serializer_class(data=request.data)
+        user = authenticate(username=request.data.get('username',None), password=request.data.get('password',None))
+        if user is not None:
+            #user = serializer.validated_data['user']
             token = TokenObtainPairSerializer.get_token(user) 
             refresh_token = str(token)
             access_token = str(token.access_token) 
             response = Response(
                 {
-                    "user": user,
                     "message": "Login Success",
                     "jwt_token": {
                         "access_token": access_token,
