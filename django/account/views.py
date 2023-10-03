c2d4c710432aea58885917dc53c9d072b4a72b2e
dydghk456 <yong@sin-yonghwaui-MacBookAir.local>
2023-10-04 05:06:40 +0900

    Fix: fix LoginView and LoginSerializer

--- django/account/views.py
+++ django/account/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from .models import CustomUser
 from .serializers import UserCreateSerializer, LoginSerializer
-from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.views import APIView
 
-# Create your views here.
+# Create your views here.   
 class SignupView(APIView):
     serializer_class = UserCreateSerializer
 
@@ -21,27 +20,22 @@ class LoginView(APIView):
     serializer_class = LoginSerializer
 
     def post(self, request):
-        #serializer = self.serializer_class(data=request.data)
-        user = authenticate(username=request.data.get('username',None), password=request.data.get('password',None))
-        if user is not None:
-            #user = serializer.validated_data['user']
-            token = TokenObtainPairSerializer.get_token(user) 
-            refresh_token = str(token)
-            access_token = str(token.access_token) 
-            response = Response(
-                {
-                    "message": "Login Success",
-                    "jwt_token": {
-                        "access_token": access_token,
-                        "refresh_token": refresh_token
-                    },
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token = TokenObtainPairSerializer.get_token(user) 
+        refresh_token = str(token)
+        access_token = str(token.access_token) 
+        response = Response(
+            {
+                "message": "Login Success",
+                "jwt_token": {
+                    "access_token": access_token,
+                    "refresh_token": refresh_token
                 },
-                status=200
-            )
+            },
+            status=200
+        )
 
-            return response
-        else:
-            return Response(
-                {"message": "Login Failed"}, status=400
-            )
-    
+        return response
+
