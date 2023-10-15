83a093b1f02dd70b86ad1985fe40675e81b803d7
김근우 <geunwookim@gimgeun-uui-MacBookPro.local>
2023-10-15 20:53:18 +0900

    Feat: Add HistoryPage UI

--- android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ui/history/HistoryViewModel.java
+++ android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ui/history/HistoryViewModel.java
@@ -1,19 +1,14 @@
 package com.example.runusandroid.ui.history;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class HistoryViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
     public HistoryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is History fragment");
     }
 
     public LiveData<String> getText() {
-        return mText;
+        return null;
     }
 }
