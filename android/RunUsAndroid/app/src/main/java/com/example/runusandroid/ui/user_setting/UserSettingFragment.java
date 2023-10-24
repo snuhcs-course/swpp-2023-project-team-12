5176e13312cb5ae49f48180f57c76961e8286888
Geunwoo Kim <geunwoo@kodebox.io>
2023-10-24 21:23:32 +0900

    Feat: Add temporary logout button

--- android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ui/user_setting/UserSettingFragment.java
+++ android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ui/user_setting/UserSettingFragment.java
@@ -1,5 +1,9 @@
 package com.example.runusandroid.ui.user_setting;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,9 +11,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.runusandroid.LoginActivity;
+import com.example.runusandroid.R;
 import com.example.runusandroid.databinding.FragmentUserSettingBinding;
 
 public class UserSettingFragment extends Fragment {
@@ -26,7 +33,29 @@ public class UserSettingFragment extends Fragment {
 
         final TextView textView = binding.textUserSetting;
         userSettingViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+        AppCompatButton logoutButton = root.findViewById(R.id.LogoutBtn); // 수정된 부분
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
+        return root;  // 이 부분이 올바른 위치로 이동되었습니다.
+    }
+
+    private void logoutUser() {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("user_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("user_id");
+        editor.apply();
+
+        // LoginActivity로 이동
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
     }
 
     @Override
