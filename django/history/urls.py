35b89179b693042656917806d545ddb8d24b2fd5
tangamzaa <tangamzaa@gmail.com>
2023-11-05 02:51:53 +0900

    fix: recent history url

--- django/history/urls.py
+++ django/history/urls.py
@@ -5,6 +5,6 @@ app_name = "history/"
 
 urlpatterns = [
   path("", HistoryDetail.as_view(), name="history_detail"),
-  path("/recent-history/<str:username>/", RecentHistory.as_view(), name="recent_history"),
+  path("recent-history/<str:username>/", RecentHistory.as_view(), name="recent_history"),
   path("group/", GroupHistoryDetail.as_view(), name = "group_history_detail")
 ]
