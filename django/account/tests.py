07cdc08edc43d6e4e0dbb7342edfabe825c70946
Geunwoo Kim <geunwoo@kodebox.io>
2023-11-11 21:14:03 +0900

    Test: Fix Invalid test

--- django/account/tests.py
+++ django/account/tests.py
@@ -1,11 +1,24 @@
-import os
-from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import AccessToken
+from django.core.files.storage import default_storage
+
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+def generate_dummy_image():
+    image = Image.new("RGB", (100, 100), (255, 255, 255))
+    image_io = BytesIO()
+    image.save(image_io, format="JPEG")
+    image_io.seek(0)
+    return SimpleUploadedFile(
+        "dummy.jpg", image_io.getvalue(), content_type="image/jpeg"
+    )
 
 
 class ProfileImageUploadTest(TestCase):
@@ -30,26 +43,28 @@ class ProfileImageUploadTest(TestCase):
         self.client.login(username="testuser", password="testpassword")
 
     def test_profile_image_upload(self):
-        """DB에 저장되는지 확인"""
-        # 업로드할 이미지 파일 생성
-        image_path = "django/media/profile_images/temp_profile.jpeg"  # 테스트용 이미지 파일 경로
-        image = SimpleUploadedFile(
-            name="test_image.jpg",
-            content=open(image_path, "rb").read(),
-            content_type="image/jpeg",
-        )
+        # 더미 이미지 생성
+        image = generate_dummy_image()
 
         # API 엔드포인트 호출
-        url = reverse("profile_image")  # 'profile_image'는 URL 패턴의 이름
+        url = reverse("profile_image")
         response = self.client.post(url, {"profile_image": image}, format="multipart")
 
         # 응답 코드 검사
         self.assertEqual(response.status_code, 200)
 
-        # 데이터베이스에 업로드된 이미지가 저장되었는지 검사
+        # 데이터베이스에 업로드된 이미지 검증
         self.user.refresh_from_db()
-        self.assertTrue(self.user.profile_image.url.endswith("test_image.jpg"))
+        self.assertTrue(self.user.profile_image.url.endswith("dummy.jpg"))
 
     def tearDown(self):
         # 데이터베이스 정리
         self.user.delete()
+
+        # 업로드된 더미 이미지 파일 삭제
+        if self.user.profile_image and default_storage.exists(
+            self.user.profile_image.name
+        ):
+            default_storage.delete(self.user.profile_image.name)
+
+        super().tearDown()
