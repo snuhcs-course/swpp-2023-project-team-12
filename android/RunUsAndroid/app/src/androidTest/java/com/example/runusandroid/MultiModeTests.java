b6ecc28fbeb184244579eea13af2c5ef23d97bf5
SNURO <no1jj@snu.ac.kr>
2023-11-18 00:35:34 +0900

    test: added multimode UI test

--- android/RunUsAndroid/app/src/androidTest/java/com/example/runusandroid/MultiModeTests.java
+++ android/RunUsAndroid/app/src/androidTest/java/com/example/runusandroid/MultiModeTests.java
@@ -1,24 +1,49 @@
 package com.example.runusandroid;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.actionWithAssertions;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.PickerActions.setTime;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.not;
+
+import static org.hamcrest.Matchers.allOf;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.view.InputDevice;
+import android.view.MotionEvent;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.GeneralClickAction;
+import androidx.test.espresso.action.GeneralLocation;
+import androidx.test.espresso.action.GeneralSwipeAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Swipe;
+import androidx.test.espresso.action.Tap;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
@@ -36,7 +61,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class SingleModeTests {
+public class MultiModeTests {
     public static ActivityScenario<LoginActivity> loginActivityScenario;
     public ActivityScenario<MainActivity2> mainActivityScenario;
     @Rule
@@ -61,6 +86,13 @@ public class SingleModeTests {
 //    public ActivityScenarioRule<MainActivity2> mActivityRule =
 //            new ActivityScenarioRule<>(MainActivity2.class);
 
+    private ViewAction clickTopCentre = actionWithAssertions(new GeneralClickAction(
+            Tap.SINGLE, GeneralLocation.TOP_CENTER, Press.FINGER, InputDevice.SOURCE_UNKNOWN, MotionEvent.BUTTON_PRIMARY));
+    private ViewAction clickBottomCentre = actionWithAssertions(new GeneralClickAction(
+            Tap.SINGLE, GeneralLocation.BOTTOM_CENTER, Press.FINGER, InputDevice.SOURCE_UNKNOWN, MotionEvent.BUTTON_PRIMARY));
+    private ViewAction swipeDown = actionWithAssertions(new GeneralSwipeAction(
+            Swipe.FAST, GeneralLocation.VISIBLE_CENTER, GeneralLocation.BOTTOM_CENTER, Press.FINGER));
+
 
     @BeforeClass
     public static void beforeClass() throws InterruptedException{
@@ -80,7 +112,7 @@ public class SingleModeTests {
 
         mainActivityScenario = ActivityScenario.launch(MainActivity2.class);
         mainActivityScenario.onActivity(activity -> {
-            activity.navController.navigate(R.id.navigation_single_mode);
+            activity.navController.navigate(R.id.navigation_multi_mode);
         });
     }
 
@@ -90,76 +122,42 @@ public class SingleModeTests {
     }
 
     @Test
-    public void startButton_dialog_selection() throws InterruptedException {
+    public void roomCreation() throws InterruptedException {
         Thread.sleep(1000);
-        // perform action and check (click on start)
-        onView(withId(R.id.startButton)).perform(click());
-        onView(withId(R.id.buttonTimeAttack)).check(matches(isDisplayed()));
-
-        // check time attack button
-        onView(withId(R.id.buttonTimeAttack)).perform(click());
-        onView(withId(R.id.buttonConfirm)).check(matches(isDisplayed()));
-        onView(withId(R.id.buttonClose)).perform(click());
-
-        // TODO: 다른 dialog select 시
-
-//        // check time attack button ...
-//        onView(withId(R.id.buttonTimeAttack)).perform(click());
-//        onView(withId(R.id.buttonConfirm)).check(matches(isDisplayed()));
-//        onView(withId(R.id.buttonClose)).perform(click());
-    }
-
-    @Test
-    public void playing() throws InterruptedException {
-        Thread.sleep(1000);
-        // enter playing screen
-        onView(withId(R.id.startButton)).perform(click());
-        onView(withId(R.id.buttonTimeAttack)).perform(click());
-        onView(withId(R.id.buttonConfirm)).perform(click());
-        onView(withId(R.id.quitButton)).check(matches(isDisplayed()));
-        Thread.sleep(2000);
-        // check if time is running
-        onView(withId(R.id.currentTimeText)).check(matches(not(withText("00:00:00"))));
-        // check if back button warning works
-        onView(isRoot()).perform(pressBack());
-        onView(withId(R.id.buttonConfirmClose)).check(matches(isDisplayed()));
-        onView(isRoot()).perform(pressBack());
-        // 종료
-        onView(withId(R.id.quitButton)).perform(click());
-        onView(withId(R.id.buttonConfirmClose)).check(matches(isDisplayed()));
-        onView(withId(R.id.buttonConfirmClose)).perform(click());
-        Thread.sleep(1000);
-        onView(anyOf(withId(R.id.buttonConfirmFailure),withId(R.id.buttonConfirm))).perform(click());
-        Thread.sleep(1000);
-
-        // 종료시 레벨업할수도 있음
-        try{
-            onView(withText("레벨 업!")).check(matches(isDisplayed()));
-            onView(withId(R.id.buttonConfirm)).perform(click());
-        } catch (Exception e) {
+        String roomName = "UITestRoom";
+        String enterMemberNum = "5.5";
+        String checkMemberNum = "55";
+        Calendar calendar;
+        // 방 생성
+        onView(withId(R.id.createRoomButton)).perform(click());
+        onView(withId(R.id.roomCreateTitle)).check(matches(isDisplayed()));
+        // swipe as hell
+        for (int i = 0; i < 29; i++) {
+            onView(withId(R.id.minutePicker)).perform(swipeDown);
+            Thread.sleep(50);
         }
+        onView(withId(R.id.editTextGroupName)).perform(replaceText(roomName));
+        onView(withId(R.id.editTextMembers)).perform(clearText());
+        onView(withId(R.id.editTextMembers)).perform(typeText(enterMemberNum)).perform(closeSoftKeyboard());
+        onView(withId(R.id.editTextMembers)).check(matches(withText(checkMemberNum)));
+        calendar = Calendar.getInstance();
+        onView(isAssignableFrom(TimePicker.class)).perform(setTime(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE) + 2));
+        onView(withId(R.id.buttonComplete)).perform(click());
 
-        // 종료하고 처음 화면으로 돌아감
-        onView(withId(R.id.resultText)).check(matches(isDisplayed()));
-        onView(withId(R.id.quitButton)).perform(click());
-        onView(withId(R.id.buttonConfirmPlayExit)).perform(click());
-        onView(withId(R.id.startButton)).check(matches(isDisplayed()));
+        // 대기중 화면에서 생성된 정보 확인
+        Thread.sleep(1000 * 60);
+        onView(withId(R.id.time_remaining)).check(matches(not(withText("잔여 시간"))));
+        onView(withId(R.id.participant_count)).check(matches(withText(endsWith(checkMemberNum))));
+        onView(withId(R.id.multi_room_wait_title)).check(matches(withText(roomName)));
+
+        // 게임중 화면에서 확인
+        Thread.sleep(1000 * 60);
+        onView(withId(R.id.play_leaveButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.time_present_content)).check(matches(not(withText("00:00:00"))));
+
+        // TODO: 게임결과 화면 확인되지 않음
     }
 
-//    public void navSingle_click_displayed2() {
-//        // Start the screen of your activity.
-//        onView(withId(R.id.nav_view))
-//                .perform(NavigationViewActions.navigateTo(R.id.navigation_single_mode));
-//        onView(withId(R.id.startButton)).perform(click());
-//    }
-//
-//    @Test
-//    public void draft(){
-//        // perform action (click)
-//        onView(withId(R.id.navigation_single_mode)).perform(click());
-//        onView(withId(R.id.startButton)).perform(click());
-//
-//        //check if UI changed
-//        onView(withId(R.id.buttonTimeAttack)).check(matches(isDisplayed()));
-//    }
+
 }
+
