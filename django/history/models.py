37518cc8f188ccfd5e5be42223200ff9872cbeb5
Geunwoo Kim <geunwoo@kodebox.io>
2023-11-29 19:17:31 +0900

    Refactor: Rename History Model to Pascal Case

--- django/history/models.py
+++ django/history/models.py
@@ -1,37 +1,35 @@
 from django.db import models
-from account.models import CustomUser
+
 
 # Create your models here.
-class history_record(models.Model):
-  user_id = models.BigIntegerField(null=False)
-  distance = models.FloatField(null=False, blank=False)
-  duration = models.DurationField()
-  is_completed = models.BooleanField(null=False, blank=False)
-  start_time = models.DateTimeField(null=False, blank=False)
-  finish_time = models.DateTimeField(null=False, blank=False)
-  calories = models.FloatField(null=True, blank=True)
-  is_group = models.BooleanField(null=False, blank=False)
-  max_speed = models.FloatField(null=True, blank=True)
-  min_speed = models.FloatField(null=True, blank=True)
-  median_speed = models.FloatField(null=True, blank=True)
-  group_history_id = models.BigIntegerField(null=True, blank=True)
-  sectional_speed = models.TextField()
-  is_mission_succeeded = models.IntegerField(default=0)
-  
-  def __str__(self):
-    return self.user_id
+class HistoryRecord(models.Model):
+    user_id = models.BigIntegerField(null=False)
+    distance = models.FloatField(null=False, blank=False)
+    duration = models.DurationField()
+    is_completed = models.BooleanField(null=False, blank=False)
+    start_time = models.DateTimeField(null=False, blank=False)
+    finish_time = models.DateTimeField(null=False, blank=False)
+    calories = models.FloatField(null=True, blank=True)
+    is_group = models.BooleanField(null=False, blank=False)
+    max_speed = models.FloatField(null=True, blank=True)
+    min_speed = models.FloatField(null=True, blank=True)
+    median_speed = models.FloatField(null=True, blank=True)
+    group_history_id = models.BigIntegerField(null=True, blank=True)
+    sectional_speed = models.TextField()
+    is_mission_succeeded = models.IntegerField(default=0)
 
-class group_history_record(models.Model):
-  roomname = models.TextField(null=False, blank=True)
-  start_time = models.DateTimeField(null=False, blank=False)
-  duration = models.DurationField()
-  num_players = models.IntegerField(null=False, blank=False)
-  first_place_user_id = models.IntegerField(null=True, blank=True)
-  first_place_user_distance = models.FloatField(null=True, blank=True)
-  second_place_user_id = models.IntegerField(null=True, blank=True)
-  second_place_user_distance = models.FloatField(null=True, blank=True)
-  third_place_user_id = models.IntegerField(null=True, blank=True)
-  third_place_user_distance = models.FloatField(null=True, blank=True)
+    def __str__(self):
+        return self.user_id
 
 
-
+class GroupHistoryRecord(models.Model):
+    roomname = models.TextField(null=False, blank=True)
+    start_time = models.DateTimeField(null=False, blank=False)
+    duration = models.DurationField()
+    num_players = models.IntegerField(null=False, blank=False)
+    first_place_user_id = models.IntegerField(null=True, blank=True)
+    first_place_user_distance = models.FloatField(null=True, blank=True)
+    second_place_user_id = models.IntegerField(null=True, blank=True)
+    second_place_user_distance = models.FloatField(null=True, blank=True)
+    third_place_user_id = models.IntegerField(null=True, blank=True)
+    third_place_user_distance = models.FloatField(null=True, blank=True)
