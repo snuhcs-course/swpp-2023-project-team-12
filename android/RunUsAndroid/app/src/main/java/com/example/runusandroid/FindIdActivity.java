33b0b94bf4a27e034d790ee107de27bc276f7345
dydghk <dydghk456@naver.com>
2023-12-02 21:56:18 +0900

    Fix:minor fix

--- android/RunUsAndroid/app/src/main/java/com/example/runusandroid/FindIdActivity.java
+++ android/RunUsAndroid/app/src/main/java/com/example/runusandroid/FindIdActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,7 @@ public class FindIdActivity extends AppCompatActivity {
     private EditText emailInput;
     private Button findIdButton;
     private AccountApi accountApi;
+    private long completeButtonLastClickTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,54 +34,55 @@ public class FindIdActivity extends AppCompatActivity {
 
         accountApi = RetrofitClient.getClient().create(AccountApi.class);
 
-        findIdButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = emailInput.getText().toString();
-                EmailData emailData = new EmailData(email);
-
-                Call<ResponseBody> call = accountApi.findUsername(emailData);
-// Retrofit 호출 결과 처리
-                call.enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        if (response.isSuccessful()) {
-                            // 성공적인 응답 처리: 이메일 전송 성공 대화 상자 표시
-                            AlertDialog.Builder builder = new AlertDialog.Builder(FindIdActivity.this);
-                            builder.setTitle("이메일 전송 완료");
-                            builder.setMessage("아이디를 입력하신 이메일로 보내드렸어요!");
-                            builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    // 로그인 액티비티로 이동
-                                    Intent intent = new Intent(FindIdActivity.this, LoginActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            });
-                            AlertDialog dialog = builder.create();
-                            dialog.show();
-                        } else {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(FindIdActivity.this);
-                            builder.setTitle("찾기 실패");
-                            builder.setMessage("가입된 이메일이 없어요.");
-                            builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    // 아무런 동작을 하지 않음
-                                }
-                            });
-                            AlertDialog dialog = builder.create();
-                            dialog.show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        // 네트워크 요청 실패 처리
-                        Toast.makeText(FindIdActivity.this, "서버에 예상치 못한 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
+        findIdButton.setOnClickListener(v -> {
+            if (SystemClock.elapsedRealtime() - completeButtonLastClickTime < 2000) {
+                return;
             }
+            completeButtonLastClickTime = SystemClock.elapsedRealtime();
+            String email = emailInput.getText().toString();
+            EmailData emailData = new EmailData(email);
+
+            Call<ResponseBody> call = accountApi.findUsername(emailData);
+// Retrofit 호출 결과 처리
+            call.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    if (response.isSuccessful()) {
+                        // 성공적인 응답 처리: 이메일 전송 성공 대화 상자 표시
+                        AlertDialog.Builder builder = new AlertDialog.Builder(FindIdActivity.this);
+                        builder.setTitle("이메일 전송 완료");
+                        builder.setMessage("아이디를 입력하신 이메일로 보내드렸어요!");
+                        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // 로그인 액티비티로 이동
+                                Intent intent = new Intent(FindIdActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(FindIdActivity.this);
+                        builder.setTitle("찾기 실패");
+                        builder.setMessage("가입된 이메일이 없어요.");
+                        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // 아무런 동작을 하지 않음
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    // 네트워크 요청 실패 처리
+                    Toast.makeText(FindIdActivity.this, "서버에 예상치 못한 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
         });
 
     }
--- android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ResetPasswordActivity.java
+++ android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ResetPasswordActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +23,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
     private Button resetPasswordButton;
 
     private AccountApi accountApi;
+    private long completeButtonLastClickTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,52 +34,54 @@ public class ResetPasswordActivity extends AppCompatActivity {
         emailInput = findViewById(R.id.emailInput2);
         resetPasswordButton = findViewById(R.id.resetPasswordButton);
         accountApi = RetrofitClient.getClient().create(AccountApi.class);
-        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = usernameInput.getText().toString();
-                String email = emailInput.getText().toString();
-                ResetPasswordData data = new ResetPasswordData(username, email);
-
-
-                Call<ResponseBody> call = accountApi.resetPassword(data);
-
-                call.enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(ResetPasswordActivity.this);
-                        if (response.isSuccessful()) {
-                            builder.setTitle("성공");
-                            builder.setMessage("임시 비밀번호가 이메일로 발송되었습니다.");
-                            builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    // LoginActivity로 이동
-                                    Intent intent = new Intent(ResetPasswordActivity.this, LoginActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            });
-                        } else {
-                            builder.setTitle("실패");
-                            builder.setMessage("아이디나 이메일이 잘못되었습니다. 다시 시도해주세요.");
-                            builder.setPositiveButton("확인", null); // 단순 확인 버튼
-                        }
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        // 네트워크 오류 처리
-                        AlertDialog.Builder builder = new AlertDialog.Builder(ResetPasswordActivity.this);
-                        builder.setTitle("네트워크 오류");
-                        builder.setMessage("서버에 예상치 못한 오류가 발생했습니다.");
-                        builder.setPositiveButton("확인", null); // 단순 확인 버튼
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                    }
-                });
+        resetPasswordButton.setOnClickListener(v -> {
+            if (SystemClock.elapsedRealtime() - completeButtonLastClickTime < 2000) {
+                return;
             }
+            completeButtonLastClickTime = SystemClock.elapsedRealtime();
+
+            String username = usernameInput.getText().toString();
+            String email = emailInput.getText().toString();
+            ResetPasswordData data = new ResetPasswordData(username, email);
+
+
+            Call<ResponseBody> call = accountApi.resetPassword(data);
+
+            call.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ResetPasswordActivity.this);
+                    if (response.isSuccessful()) {
+                        builder.setTitle("성공");
+                        builder.setMessage("임시 비밀번호가 이메일로 발송되었습니다.");
+                        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // LoginActivity로 이동
+                                Intent intent = new Intent(ResetPasswordActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+                    } else {
+                        builder.setTitle("실패");
+                        builder.setMessage("아이디나 이메일이 잘못되었습니다. 다시 시도해주세요.");
+                        builder.setPositiveButton("확인", null); // 단순 확인 버튼
+                    }
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    // 네트워크 오류 처리
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ResetPasswordActivity.this);
+                    builder.setTitle("네트워크 오류");
+                    builder.setMessage("서버에 예상치 못한 오류가 발생했습니다.");
+                    builder.setPositiveButton("확인", null); // 단순 확인 버튼
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            });
         });
     }
 }
