37c980093487b7198c21fbb10f2e97db8fcdab1e
Jack-Chagarr <netamijr@gmail.com>
2023-12-03 21:11:55 +0900

    Feat: add random quotes in Home Fragment Screen

--- android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ui/home/HomeFragment.java
+++ android/RunUsAndroid/app/src/main/java/com/example/runusandroid/ui/home/HomeFragment.java
@@ -21,8 +21,8 @@ import com.example.runusandroid.RetrofitClient;
 import com.example.runusandroid.databinding.FragmentHomeBinding;
 import com.example.runusandroid.ui.multi_mode.SocketManager;
 
-import java.io.IOException;
 import java.util.Calendar;
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
 
@@ -30,8 +30,11 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private HistoryApi historyApi;
 
+    private TextView todayQuotesText;
+    private TextView todayQuotesAuthorText;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
@@ -47,6 +50,12 @@ public class HomeFragment extends Fragment {
         HomePageAdapter adapter = new HomePageAdapter();
         viewPager.setAdapter(adapter);
         TextView pageIndicator = root.findViewById(R.id.pageIndicator);
+
+        todayQuotesText = root.findViewById(R.id.todayQuotesText);
+        todayQuotesAuthorText = root.findViewById(R.id.todayQuotesAuthorText);
+        String[] quote = getRandomQuotes().split("/");
+        todayQuotesText.setText(quote[0]);
+        todayQuotesAuthorText.setText(quote[1]);
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -68,7 +77,6 @@ public class HomeFragment extends Fragment {
         pageIndicator.setText("1/" + adapter.getCount());
 
         CardView runningRecordCard = root.findViewById(R.id.runningRecordCard);
-        TextView todayRunningHistoryTextView = root.findViewById(R.id.todayRunningHistoryTextView);
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
@@ -116,6 +124,30 @@ public class HomeFragment extends Fragment {
     }
 
      */
+
+    private String getRandomQuotes() {
+        // Random 객체 생성
+        Random random = new Random();
+
+        // 1부터 10까지의 랜덤 숫자 선택
+        int randomNumber = random.nextInt(10);
+
+        String[] quotes = new String[10];
+        quotes[0] = "\n\n내가 잘 뛰는 것은 타고났다기 보다는 노력했기 때문이다. /\n\n\n\n\n- 이봉주";
+        quotes[1] = "\n\n달리기는 내게 명상이며, 순화된 정신이고, 우주와의 교류, 기분 전환제이며 영혼의 교감이다. /\n\n\n\n\n- 로레인 몰러";
+        quotes[2] = "\n\n물고기는 헤엄치고 새는 날고 인간은 달린다. /\n\n\n\n\n- 에밀 자토펙";
+        quotes[3] = "\n\n운동화 한 켤레 후다닥 신고 문 밖으로 달려 나가면, 당신이 있는 곳이 바로 여기, 자유. /\n\n\n\n\n- 좀 제론";
+        quotes[4] = "\n\n달리기는 인생에 대한 가장 위대한 비유이다. 당신이 달리기에 쏟아붓는 것을 결국 다 돌려받기 때문이다. /\n\n\n\n\n- 오프라 윈프리";
+        quotes[5] = "\n\n내가 인생에서 배워야 할 모든 것을 마라톤에서 배웠다. 마라톤은 인생 이상이다. /\n\n\n\n\n- 김봉조";
+        quotes[6] = "\n\n대회는 빠른 주자만을 위한 것이 아니다. 그것은 달리기를 지속하는 사람을 위한 것이다. /\n\n\n\n\n- 나이키";
+        quotes[7] = "\n\n그 어떤 쾌락도 \"변화 또는 다양성\"이 없다면 참기 힘들 것이다. /\n\n\n\n\n- 막심";
+        quotes[8] = "\n\n꿈을 가져라, 계획을 세워라. 그리고 그것을 향해 나아가라. 약속하건대, 그러면 당신은 거기에 이를 것이다. /\n\n\n\n\n- 조 코플로비츠";
+        quotes[9] = "\n\n기억하라. 잘 달린 뒤의 기분이 달리기를 할 생각만 하면서 하는 일 없이 지낸 뒤의 기분보다 훨씬 좋다는 것을. /\n\n\n\n\n- 사라 콘도르";
+
+        return quotes[randomNumber];
+
+    }
+
 
     @Override
     public void onDestroyView() {
