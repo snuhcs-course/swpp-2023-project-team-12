95dca6a502ec2dcc1364f80b5a87c9dffc4d11ce
SNURO <no1jj@snu.ac.kr>
2023-12-08 17:53:59 +0900

    hide keyboard applied on activities

--- android/RunUsAndroid/app/src/main/java/com/example/runusandroid/SignUpStep1Activity.java
+++ android/RunUsAndroid/app/src/main/java/com/example/runusandroid/SignUpStep1Activity.java
@@ -1,10 +1,13 @@
 package com.example.runusandroid;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.Spanned;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -106,5 +109,19 @@ public class SignUpStep1Activity extends AppCompatActivity {
                 startActivity(intentToLogin);
             }
         });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
